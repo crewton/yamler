8d7a368f1a2b5a4d97bd1640e451bf72e4f55fd9
Crew Reading <crew@instructure.com>
2017-04-21 11:35:04 -0600

    gofmt

--- main.go
+++ main.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-  "log"
-  "flag"
-  "os"
-  "io/ioutil"
-  "github.com/smallfish/simpleyaml"
+	"flag"
+	"github.com/smallfish/simpleyaml"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 func main() {
-  var filename = flag.String("f", "", "the filename to parse")
-  flag.Parse()
+	var filename = flag.String("f", "", "the filename to parse")
+	flag.Parse()
 
-  var data []byte
-  var err error
-  if *filename == "" {
-    log.Fatalln("You need to specify a filename.")
-  } else {
-    data, err = ioutil.ReadFile(*filename)
-    if err != nil {
-      log.Fatalln("Error reading file:", err)
-    }
-  }
+	var data []byte
+	var err error
+	if *filename == "" {
+		log.Fatalln("You need to specify a filename.")
+	} else {
+		data, err = ioutil.ReadFile(*filename)
+		if err != nil {
+			log.Fatalln("Error reading file:", err)
+		}
+	}
 
-  success, err := parseYaml(data)
-  if err != nil {
-    log.Fatalln("Error parsing yaml:", err)
-  }
+	success, err := parseYaml(data)
+	if err != nil {
+		log.Fatalln("Error parsing yaml:", err)
+	}
 
-  if success {
-    os.Exit(0)
-  } else {
-    os.Exit(1)
-  }
+	if success {
+		os.Exit(0)
+	} else {
+		os.Exit(1)
+	}
 }
 
 func parseYaml(input []byte) (bool, error) {
-  _, err := simpleyaml.NewYaml(input)
-  if err != nil {
-    return false, err
-  }
+	_, err := simpleyaml.NewYaml(input)
+	if err != nil {
+		return false, err
+	}
 
-  return true, nil
+	return true, nil
 }
--- yamler_test.go
+++ yamler_test.go
@@ -17,15 +17,15 @@ data:
 `)
 
 func TestGoodParser(t *testing.T) {
-  succ, err := parseYaml(goodYaml)
-  if !succ {
-    t.Errorf("Somehow it failed to parse good yaml: %s (%s)", succ, err)
-  }
+	succ, err := parseYaml(goodYaml)
+	if !succ {
+		t.Errorf("Somehow it failed to parse good yaml: %s (%s)", succ, err)
+	}
 }
 
 func TestBadParser(t *testing.T) {
-  succ, err := parseYaml(badYaml)
-  if succ {
-    t.Errorf("Somehow it successfully parsed bad yaml: %s (%s)", succ, err)
-  }
+	succ, err := parseYaml(badYaml)
+	if succ {
+		t.Errorf("Somehow it successfully parsed bad yaml: %s (%s)", succ, err)
+	}
 }
